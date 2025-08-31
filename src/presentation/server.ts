170acb4b6786a8cb93042af393c6ec890c2a12e6
Jorge Ramos <jorgeramos2068@gmail.com>
2025-08-30 18:18:55 -0600

    feat: add cron service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,10 @@
+import { CronService } from './cron/cron.service';
+
 export class Server {
   public static start() {
-    console.log('Server is started');
+    console.log('Server has started');
+    CronService.createJob('*/5 * * * * *', () => {
+      console.log('You will see this message every 5 seconds');
+    });
   }
 }
