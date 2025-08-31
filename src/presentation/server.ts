25184d490d057e1192afca6f8530f6bd1e9baf30
Jorge Ramos <jorgeramos2068@gmail.com>
2025-08-30 18:25:35 -0600

    feat: add check service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,11 @@
+import { CheckService } from '../domain/use-cases/checks/check.service';
 import { CronService } from './cron/cron.service';
 
 export class Server {
   public static start() {
     console.log('Server has started');
     CronService.createJob('*/5 * * * * *', () => {
-      console.log('You will see this message every 5 seconds');
+      new CheckService().execute('https://google.com');
     });
   }
 }
