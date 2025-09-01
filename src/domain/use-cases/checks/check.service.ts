2d58bb08f392884d8b4da2facd667e16904cb75d
Jorge Ramos <jorgeramos2068@gmail.com>
2025-09-01 14:09:07 -0600

    feat: add dependency injection for the check service

--- src/domain/use-cases/checks/check.service.ts
+++ src/domain/use-cases/checks/check.service.ts
@@ -2,17 +2,22 @@ interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+  constructor(private readonly successCallback: SuccessCallback, private readonly errorCallback: ErrorCallback) {}
+
   async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url);
       if (!req.ok) {
         throw new Error(`Failed to fetch ${url}`);
       }
-      console.log(`Successfully fetched ${url}`);
+      this.successCallback();
       return true;
     } catch (error) {
-      console.error(error);
+      this.errorCallback(`${error}`);
       return false;
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -5,7 +5,11 @@ export class Server {
   public static start() {
     console.log('Server has started');
     CronService.createJob('*/5 * * * * *', () => {
-      new CheckService().execute('https://google.com');
+      const url = 'https://google.com';
+      new CheckService(
+        () => console.log(`Successfully fetched ${url}`),
+        error => console.error(`Failed to fetch ${url}: ${error}`)
+      ).execute(url);
     });
   }
 }
