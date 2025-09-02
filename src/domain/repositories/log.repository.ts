21b6af414cf99f179d2b66500d03b448bc9f8a1c
Jorge Ramos <jorgeramos2068@gmail.com>
2025-09-02 17:46:01 -0600

    feat: add abstract classes for datasourcers and repositories

---
+++ src/domain/repositories/log.repository.ts
@@ -0,0 +1,6 @@
+import { LogEntity, LogLevel } from '../entities/log.entity';
+
+export abstract class LogRepository {
+  abstract saveLog(log: LogEntity): Promise<void>;
+  abstract getLogs(level: LogLevel): Promise<LogEntity[]>;
+}
