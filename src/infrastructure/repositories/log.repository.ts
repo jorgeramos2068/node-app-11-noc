d1933a8d7f23f0f8eae6d903eca3e139dd5ecb66
Jorge Ramos <jorgeramos2068@gmail.com>
2025-09-04 16:23:47 -0600

    feat: create implementation for log repository

---
+++ src/infrastructure/repositories/log.repository.ts
@@ -0,0 +1,15 @@
+import { LogDatasource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogLevel } from '../../domain/entities/log.entity';
+import { LogRepository } from '../../domain/repositories/log.repository';
+
+export class LogRepositoryImpl implements LogRepository {
+  constructor(private readonly logDatasource: LogDatasource) {}
+
+  async saveLog(log: LogEntity): Promise<void> {
+    this.logDatasource.saveLog(log);
+  }
+
+  async getLogs(level: LogLevel): Promise<LogEntity[]> {
+    return this.logDatasource.getLogs(level);
+  }
+}
