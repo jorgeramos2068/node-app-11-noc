d8e85806eabbc2c701c917dc61692845c81c76af
Jorge Ramos <jorgeramos2068@gmail.com>
2025-09-25 16:27:30 -0600

    feat: add MongoDB configuration and packages

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import { envs } from '@/config/plugins/env.plugin';
+import { MongoDatabase } from '@/data/mongo';
 import { Server } from '@/presentation/server';
 
 (async () => {
-  main();
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+  // main();
 })();
 
 function main() {
---
+++ src/data/mongo/init.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+interface ConnectionOptions {
+  mongoUrl: string;
+  dbName: string;
+}
+
+export class MongoDatabase {
+  public static async connect(options: ConnectionOptions) {
+    const { mongoUrl, dbName } = options;
+    try {
+      await mongoose.connect(mongoUrl, {
+        dbName,
+      });
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.log('Error connecting to MongoDB:', error);
+      throw error;
+    }
+  }
+}
